1e951239cb2dc3f2b8336f9bf40b037bb3107eb4
Sergey Cheparev <sergey@cheparev.com>
2020-04-17 17:41:58 +0200

    Building the url and passing the result

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 import os
 
+import requests
+from dotenv import load_dotenv
 from flask import Flask, render_template, request
+from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
 
-from dotenv import load_dotenv
-
 load_dotenv()
-CIRCLE_API_URL = os.getenv("CIRCLE_API_URL")
+VCS_TYPE = os.getenv("VCS_TYPE")
+ORG = os.getenv("ORG")
+REPO = os.getenv("REPO")
 CIRCLE_API_KEY = os.getenv("CIRCLE_API_KEY")
 CIRCLE_JOB_NAME = os.getenv("CIRCLE_JOB_NAME")
 
 
 @app.route('/', methods=['GET', 'POST'])
 def forward():
-    print(CIRCLE_JOB_NAME, CIRCLE_API_KEY, CIRCLE_API_URL)
-    return render_template('forward.html')
+    context = {}
+    if request.method == 'POST':
+        branch = request.form['head']
+        url = f"https://circleci.com/api/v1.1/project/{VCS_TYPE}/{ORG}/{REPO}/tree/{branch}"
+        response = requests.post(
+            url,
+            auth=HTTPBasicAuth(CIRCLE_API_KEY, ''),
+            data={
+                "build_parameters[CIRCLE_JOB]": CIRCLE_JOB_NAME
+            }
+        )
+        context['response'] = response.json()
+    return render_template('forward.html', **context)
