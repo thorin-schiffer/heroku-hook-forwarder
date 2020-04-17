9bbaba4508b13084823d56fe6fdbe5d134c4d990
Sergey Cheparev <sergey@cheparev.com>
2020-04-17 17:18:23 +0200

    Pass config over dotenv

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # heroku-hook-forwarder
 Heroku http deploy hook forward to circle ci API
+
+
+see .env_template for env variables
--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import os
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+from dotenv import load_dotenv
+
+load_dotenv()
+CIRCLE_API_URL = os.getenv("CIRCLE_API_URL")
+CIRCLE_API_KEY = os.getenv("CIRCLE_API_KEY")
+CIRCLE_JOB_NAME = os.getenv("CIRCLE_JOB_NAME")
+
 
 @app.route('/', methods=['GET', 'POST'])
 def forward():
-    print(request.form)
+    print(CIRCLE_JOB_NAME, CIRCLE_API_KEY, CIRCLE_API_URL)
     return render_template('forward.html')
