b39b7db05280b6276b50933c1fa6f4c47e2c9ac7
Sergey Cheparev <sergey@cheparev.com>
2020-04-20 14:54:27 +0200

    Added branch lookup on public github repos

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # heroku-hook-forwarder
 Heroku http deploy hook forward to circle ci API
 
-
 see .env_template for env variables
+
+* warning * heroku http hook doesn't deliver the branch name, so the lookup for sha1 -> branch name is made manually.
+Branch lookup is only suppored for Github public repos for now, PRs welcome for other repos
+ 
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 from dotenv import load_dotenv
 from flask import Flask, render_template, request
 from requests.auth import HTTPBasicAuth
+from github import Github
 
 app = Flask(__name__)
 
@@ -15,13 +16,29 @@ CIRCLE_API_KEY = os.getenv("CIRCLE_API_KEY")
 CIRCLE_JOB_NAME = os.getenv("CIRCLE_JOB_NAME")
 
 
+def resolve_branch_name(sha1):
+    if VCS_TYPE != 'github':
+        print("Don't know how to resolve sha1 to branch for non github repos")
+    g = Github()
+    repo = g.get_repo(f"{ORG}/{REPO}")
+    branches = repo.get_branches()
+    for branch in branches:
+        if branch.commit.sha == sha1:
+            return branch.name
+
+
 @app.route('/', methods=['GET', 'POST'])
 def forward():
     context = {}
     if request.method == 'POST':
         sha1_head = request.form['head_long']
         review_url = request.form['url']
-        url = f"https://circleci.com/api/v1.1/project/{VCS_TYPE}/{ORG}/{REPO}"
+        branch = resolve_branch_name(sha1_head)
+        if branch:
+            url = f"https://circleci.com/api/v1.1/project/{VCS_TYPE}/{ORG}/{REPO}/tree/{branch}"
+        else:
+            url = f"https://circleci.com/api/v1.1/project/{VCS_TYPE}/{ORG}/{REPO}"
+        print(f"Redirecting hook to {url}")
         response = requests.post(
             url,
             auth=HTTPBasicAuth(CIRCLE_API_KEY, ''),
@@ -34,3 +51,7 @@ def forward():
         print(response.json(), request.form)
         context['response'] = response.json()
     return render_template('forward.html', **context)
+
+
+if __name__ == '__main__':
+    print(resolve_branch_name("ed08fd17ef1e5199e9d34f48f1358416ba30fe54"))
